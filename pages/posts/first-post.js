423be87a6c5257c49ed97693e3d3889c5125c3a6
TonyInBeijing <yuehaowei1126@gmail.com>
2022-08-01 21:54:12 +0800

    feat: add one new page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: TonyInBeijing
+ * @Date: 2022-08-01 21:41:41
+ * @LastEditors: TonyInBeijing
+ * @LastEditTime: 2022-08-01 21:45:14
+ * @FilePath: /nextjs-project/pages/posts/first-post.js
+ * @Description: first post page
+ * 
+ */
+export default function FirstPost() {
+    return <h1>First Post</h1>;
+}
