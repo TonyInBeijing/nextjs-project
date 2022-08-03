7a3e6450f6a65e28a95ed40166ebe873c1729fa9
TonyInBeijing <yuehaowei1126@gmail.com>
2022-08-03 23:05:46 +0800

    feat: getStaticProps

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,11 +2,22 @@
  * @Author: TonyInBeijing
  * @Date: 2022-08-01 21:41:41
  * @LastEditors: TonyInBeijing
- * @LastEditTime: 2022-08-01 21:45:14
+ * @LastEditTime: 2022-08-03 23:03:47
  * @FilePath: /nextjs-project/pages/posts/first-post.js
  * @Description: first post page
  * 
  */
-export default function FirstPost() {
-    return <h1>First Post</h1>;
+
+// TODO: 获取博文内容，会在构建时调用
+export async function getStaticProps() {
+    const requestRes = await fetch("xxxx.com/content/getcontent");
+    return {
+        props: {
+            postContent: requestRes
+        }
+    }
+};
+
+export default function FirstPost({ postContent }) {
+    return postContent;
 }
